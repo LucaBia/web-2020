024662251c9c66fb95e7795c0088c7f29fcaed80
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-05 23:25:00 -0600

    Viewset of owner

--- ejemplos_django/example/owners/views.py
+++ ejemplos_django/example/owners/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from owners.models import Owner
+from owners.serializers import OwnerSerializer
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer 
